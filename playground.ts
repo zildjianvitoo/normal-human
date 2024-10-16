2cfbc64e4a5e0bb7ee007368457317acdbb29a3d
zildjianvitoo <zildjianvito07@gmail.com>
2024-10-16 18:23:08 +0700

    build:404

--- playground.ts
+++ playground.ts
@@ -2,8 +2,8 @@ import { db } from "@/server/db";
 
 await db.user.create({
   data: {
-    email: "kocak@gmail.com",
-    first_name: "nama depn",
-    last_name: "nama belakang",
+    email: "kocak@gmail.casasom",
+    first_name: "nama de pn",
+    last_name: "nama asasasbelakang",
   },
 });
