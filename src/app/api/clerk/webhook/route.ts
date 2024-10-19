ea8d48e5e1a192e4c45b797070fbf1a0e897a23a
zildjianvitoo <zildjianvito07@gmail.com>
2024-10-19 21:35:50 +0700

    build: test log if error

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -3,23 +3,27 @@
 import { db } from "@/server/db";
 
 export async function POST(req: Request) {
-  const { data } = await req.json();
+  try {
+    const { data } = await req.json();
 
-  const id = data.id;
-  const email = data.email_addresses[0].email_address;
-  const firstName = data.first_name;
-  const lastName = data.last_name;
-  const imageUrl = data.image_url;
+    const id = data.id;
+    const email = data.email_addresses[0].email_address;
+    const firstName = data.first_name;
+    const lastName = data.last_name;
+    const imageUrl = data.image_url;
 
-  await db.user.create({
-    data: {
-      id,
-      emailAddress: email,
-      firstName: firstName,
-      lastName: lastName,
-      imageUrl: imageUrl,
-    },
-  });
+    await db.user.create({
+      data: {
+        id,
+        emailAddress: email,
+        firstName: firstName,
+        lastName: lastName,
+        imageUrl: imageUrl,
+      },
+    });
 
-  return new Response("Webhook diterima", { status: 200 });
+    return new Response("Webhook diterima", { status: 200 });
+  } catch (error) {
+    console.log(error);
+  }
 }
