4ae5f66f1848c271dd35124c1b2898786fa0b966
zildjianvitoo <zildjianvito07@gmail.com>
2024-10-19 19:21:10 +0700

    build: change

--- playground.ts
+++ playground.ts
@@ -3,7 +3,7 @@ import { db } from "@/server/db";
 await db.user.create({
   data: {
     email: "kocak@gmail.casasom",
-    first_name: "nama de pn perubahan  ",
+    first_name: "nama de pn perub  ahan  ",
     last_name: "nama asasasbelakang",
   },
 });
